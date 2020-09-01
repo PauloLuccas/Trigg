3c981260f88e36503041c75f6687d47247656b2f
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2020-09-01 09:15:41 -0300

    Adicionei mais conteúdo na segunda página, falta deixa-lá responsiva.

--- src/pages/theTrigg/theTrigg.js
+++ src/pages/theTrigg/theTrigg.js
@@ -6,6 +6,7 @@ import logo from '../../componentes/assets/images/logo-trigg.png';
 import imgIcon from '../../componentes/assets/images/Shape.svg';
 import imgMenu from '../../componentes/assets/images/menu.png';
 import imgTrigg from '../../componentes/assets/images/img_about.png';
+import check from '../../componentes/assets/images/tick.svg';
 
 import './style.css';
 
@@ -27,7 +28,31 @@ function theTrigg() {
                     </div>
 
                     <div className="text-hello">
-                        <h1>Olá, nós somos a Trigg ;)</h1>
+                        <h1>Olá, nós somos <br/> a <span> Trigg ;) </span></h1>
+                        <p className="subtitle">Vivemos de <span>tecnologia!</span></p>
+
+                        <div className="about-text">
+                            <div className="text-flex">
+                                <img src={check} alt="Check"/>
+                                <p>Compartilhamos nossa cultura e aprendizados</p>
+                            </div>
+                            
+                            <div className="text-flex">
+                                <img src={check} alt="Check"/>
+                                <p>Estamos sempre em busca do diferente</p>
+                            </div>
+
+                            <div className="text-flex">
+                                <img src={check} alt="Check"/>
+                                <p>Queremos transformar o mercado financeiro com tecnologia digital</p>
+                            </div>
+                            
+                            <div className="text-flex end">
+                                <img src={check} alt="Check"/>
+                                <p>Trabalhamos para descomplicar sua vida financeira</p>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </section>
