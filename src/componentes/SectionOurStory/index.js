20e87da41f5f4ce0b35db8daed01c565740d69fa
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2020-09-11 06:35:52 -0300

    Fiz algumas alterações no HTML, CSS3 e JS para o slider do site.

--- src/componentes/SectionOurStory/index.js
+++ src/componentes/SectionOurStory/index.js
@@ -10,8 +10,21 @@ export default function SectionOurStory() {
                 <h1>Nossa <span>história</span></h1>
             </div>
 
+            <h2 className="years">2017</h2>
+
             <div className="sliderStory">
-                Slider
+                <div class="slider-story">
+                    <div class="slider__prev"></div>
+                    <div class="slider__next"></div>
+                    <div class="slider__stream">
+                        <div class="slider__item bg-1"></div>
+                        <div class="slider__item bg-2"></div>
+                        <div class="slider__item bg-3">
+                            teste
+                        </div>
+                        <div class="slider__item bg-4"></div>
+                    </div>
+                </div>
             </div>
         </section>
     )
---
+++ src/controllers/SliderCarousel/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SliderCarousel(props) {
+    return (
+        <div className="containers">
+            
+        </div>
+    )
+} 
