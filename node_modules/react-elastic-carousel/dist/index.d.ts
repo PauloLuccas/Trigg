43ce7cd89f39a9a060e74e95898ad69173b475c8
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2020-09-14 09:44:41 -0300

    Adicionei o slider carousel na página Conheça a trigg.

---
+++ node_modules/react-elastic-carousel/dist/index.d.ts
@@ -0,0 +1,105 @@
+import * as React from "react";
+
+type RenderArrowProps = {
+  type: "PREV" | "NEXT";
+  onClick: () => void;
+  isEdge: boolean;
+};
+
+type RenderPaginationProps = {
+  pages: number;
+  activePage: number;
+  // The onClick event that sets the state of the carousel and sends
+  // it to a specific page.
+  onClick: (indicatorId: string) => void;
+};
+
+type ItemObject = {
+  // Children's props
+  object: any;
+  index: number;
+};
+
+type Breakpoint = {
+  itemsToScroll: number;
+  itemsToShow: number;
+};
+
+export interface ReactElasticCarouselProps {
+  className?: string;
+  // Defaults to 1
+  itemsToShow?: number;
+  // Defaults to false
+  verticalMode?: boolean;
+  // Defaults to true
+  pagination?: boolean;
+  // Defaults to 500
+  transitionMs?: number;
+  // Defaults to "ease"
+  easing?: string;
+  // Defaults to "ease"
+  tiltEasing?: string;
+  // Defaults to true.
+  enableTilt?: boolean;
+  // Defaults to 1
+  itemsToScroll?: number;
+  // Collection of objects with a width, itemsToShow and itemsToScroll
+  breakPoints?: {
+    width: number;
+    itemsToShow?: number;
+    itemsToScroll?: number;
+  }[];
+  // Defaults to 0
+  initialFirstItem?: number;
+  // Defaults to true
+  showArrows?: boolean;
+  // Defaults to true
+  disableArrowsOnEnd?: boolean;
+  // Defaults to boolean
+  focusOnSelect?: boolean;
+  // Function to generate your own navigation arrows.
+  renderArrow?: (props: RenderArrowProps) => void;
+  // Function to generate your own pagination component.
+  renderPagination?: (props: RenderPaginationProps) => JSX.Element;
+  // Defaults to "CENTER"
+  itemPosition?: "START" | "CENTER" | "END";
+  // A padding for each element - Defaults to [0,0,0,0]
+  itemPadding?: number[];
+  // Enable or disable swipe - Defaults to true
+  enableSwipe?: boolean;
+  /** Enable or disable mouse swipe */
+  enableMouseSwipe?: boolean;
+  /** Prevent page scroll on touchmove.
+   * Use this to stop the browser from scrolling while a user swipes.
+   * More details: https://github.com/FormidableLabs/react-swipeable#preventdefaulttouchmoveevent-details
+   */
+  preventDefaultTouchmoveEvent?: boolean;
+  // Enable or disable auto play - Defaults to true
+  enableAutoPlay?: boolean;
+  /** Set auto play speed (ms) - Defaults to 2000 */
+  autoPlaySpeed?: number;
+  // A callback for the change of an item
+  onChange?: (currentItemObject: ItemObject, currentPageIndex: number) => void;
+  //  A callback for the beginning of the next transition
+  onNextStart?: (
+    prevItemObject: ItemObject,
+    nextItemObject: ItemObject
+  ) => void;
+  // A callback for the beginning of the prev transition
+  onPrevStart?: (
+    prevItemObject: ItemObject,
+    nextItemObject: ItemObject
+  ) => void;
+  //  A callback for the end of the next transition
+  onNextEnd?: (nextItemObject: ItemObject, currentPageIndex: number) => void;
+  //  A callback for the end of the prev transition
+  onPrevEnd?: (nextItemObject: ItemObject, currentPageIndex: number) => void;
+  // A callback for the "slider-container" resize
+  onResize?: (currentBreakpoint: Breakpoint) => void;
+}
+
+declare class ReactElasticCarousel extends React.Component<
+  ReactElasticCarouselProps
+> {}
+
+export default ReactElasticCarousel;
--- src/componentes/SectionOurStory/index.js
+++ src/componentes/SectionOurStory/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Carousel from 'react-elastic-carousel';
 
 import './style.css';
 
 export default function SectionOurStory() {
+
     return (
         <section className="OurStory">
             
@@ -10,21 +12,102 @@ export default function SectionOurStory() {
                 <h1>Nossa <span>história</span></h1>
             </div>
 
-            <h2 className="years">2017</h2>
-
-            <div className="sliderStory">
-                <div class="slider-story">
-                    <div class="slider__prev"></div>
-                    <div class="slider__next"></div>
-                    <div class="slider__stream">
-                        <div class="slider__item bg-1"></div>
-                        <div class="slider__item bg-2"></div>
-                        <div class="slider__item bg-3">
-                            teste
+            <div className="Story">
+                <Carousel>
+                    <div className="sliderStory">
+                        
+                        <div>
+                            <h2>2017</h2>
+                            <p>
+                                <span>Março:</span><br/>
+                                Nascemos com um cartão de crédito totalmente digital,
+                                com Cashback, Cartão Vitual, opção de saques e muito 
+                                outros benefícios!
+                            </p>
+                            <p>
+                                <span>Setembro:</span><br/>
+                                Pensando em uma maneira mais moderna de usar o cartão 
+                                de crédito, lançamos a Band, a pulseira de pensamento Trigg.
+                            </p>
+                            <p>
+                                <span>Novembro:</span><br/>
+                                Para dar aquela forcinha para os clientes Trigg, lançamos o
+                                +Grana, uma graninha pré aprovada para saques que pode ser
+                                contratada direto no App.
+                            </p>
+                            <p>
+                                <span>Dezembro:</span><br/>
+                                Para encerrar o ano em grande estilo, liberamos o The Game,
+                                o joguinho da Trigg perfeito para divertir a galera e de quebra
+                                laçamos uma assistência para cães e gatos, o Meu Pet.                       
+                            </p>
                         </div>
-                        <div class="slider__item bg-4"></div>
+                        
                     </div>
-                </div>
+
+                    <div className="sliderStory">
+                        
+                        <div>
+                            <h2>2018</h2>
+                            <p>
+                                <span>Abril:</span><br/>
+                                Entrando no ritmo do maior campeonato de futebol do mundo, 
+                                lançamos o Cartão Comemorativo em versão limitada com as cores 
+                                do Brasil para dar aquela animada na torcida!
+                            </p>
+                            <p>
+                                <span>Outubro:</span><br/>
+                                Lançamos 3 cartões em parceria com a DC Comics com artes do Batman,
+                                Mulher Maravilha e Superman.
+                            </p>
+                            <p>
+                                <span>Dezembro:</span><br/>
+                                Fomos o cartão de crédito oficial da CCXP18 e para completar, lançamos
+                                o cartão do Aguaman!
+                            </p>
+                        </div>
+                        
+                    </div>
+
+                    <div className="sliderStory">
+                        
+                        <div>
+                            <h2>2019</h2>
+                            <p>
+                            <span>Março:</span><br/>
+                                Implementamos a teclonologia NFC em todos os modelos de cartão
+                                e o nosso trio de heróis ganhou o prêmio inovação Warner!
+                            </p>
+                            <p>
+                                <span>Setembro:</span><br/>
+                                Para deixar o time ainda maior, foi a vez do Joker ter um cartão
+                                para chamar de seu.
+                            </p>
+                            <p>
+                            <span>Dezembro:</span><br/>
+                                Para fechar 2019 com tudo, além na nossa participação épica como 
+                                cartão de crédito oficial da CCXP19, lançamos o cartão da Arlequina!                        
+                            </p>
+                        </div>
+                        
+                    </div>
+
+                    <div className="sliderStory">
+                        
+                        <div>
+                            <h2>2020</h2>
+                            <p>
+                                <span>Março:</span><br/>
+                                Começamos lançando o Cartão Adicional para a galera acima de 8 anos!
+                            </p>
+                            <p>
+                                <span>Maio:</span><br/>
+                                Lançamento do Cash Protegido, um seguro que protege o bolso e o cartão.
+                            </p>
+
+                        </div>
+                    </div>
+                </Carousel>
             </div>
         </section>
     )
