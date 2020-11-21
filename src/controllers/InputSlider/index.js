b6cef917dd8fcf40b4f3078fb9cb55082918f15c
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2020-11-21 17:19:40 -0300

    Projeto Finalizado.

--- src/controllers/InputSlider/index.js
+++ src/controllers/InputSlider/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Slider from "@material-ui/core/Slider";
-import Tooltip from "@material-ui/core/Tooltip";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,22 +10,6 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(3)
   }
 }));
-
-function ValueLabelComponent(props) {
-  const { children, open, value } = props;
-
-  return (
-    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
-      {children}
-    </Tooltip>
-  );
-}
-
-ValueLabelComponent.propTypes = {
-  children: PropTypes.element.isRequired,
-  open: PropTypes.bool.isRequired,
-  value: PropTypes.number.isRequired
-};
 
 const PrettoSlider = withStyles({
   root: {
